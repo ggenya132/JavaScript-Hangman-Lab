8a7e1ac867213a5df0d431a6328ba006d4f1b033
Genya Vedensky <eugene.vedensky@gmail.com>
2021-01-25 20:07:12 -0500

    adding stuff

--- Hangman.js
+++ Hangman.js
@@ -1,20 +1,67 @@
-
 class Hangman {
+  constructor(message) {
+    this.message = message;
+    this.guesses = 6;
+    this.guessedLetters = [];
+  }
+  getDisplayMessage() {
+    let resultOfAddingAsteriks = "";
+    const allTheLetters = this.message.split("");
 
-    getDisplayMessage(){
-        return null;
+    for (let i = 0; i < allTheLetters.length; i++) {
+      const currentLetter = allTheLetters[i];
+      if (this.guessedLetters.includes(currentLetter)) {
+        resultOfAddingAsteriks += currentLetter;
+      } else if (currentLetter !== " ") {
+        resultOfAddingAsteriks += "*";
+      } else {
+        resultOfAddingAsteriks += " ";
+      }
     }
 
-    guessLetter(letter){}
+    return resultOfAddingAsteriks;
+  }
 
-    getRemainingGuesses(){
-        return -1;
+  guessLetter(letter) {
+    const upperCasedLetter = letter.toUpperCase();
+    console.log(upperCasedLetter, letter);
+    const splitPhrase = this.message.split("");
+
+    // COMPARE APPLES TO APPLES, make sure you compare upper case to upper case
+    const letterGuessedIsInPhrase = splitPhrase
+      .map(function (currentLetter) {
+        return currentLetter.toUpperCase();
+      })
+      .includes(upperCasedLetter);
+
+    if (letterGuessedIsInPhrase) {
+      this.guessedLetters.push(letter);
+    } else {
+      this.guesses--;
     }
 
-    isMessageSolved(){
-        return false;
-    }
+    // see if phrase includes letter that was guesses
+    // record what letters have been guessed
 
+    // decrement remaining guesses
+  }
+
+  getRemainingGuesses() {
+    return this.guesses;
+  }
+
+  isMessageSolved() {
+    let isSolved = true;
+    const splitMessage = this.getDisplayMessage().split("");
+    for (let i = 0; i < splitMessage.length; i++) {
+      let currentLetter = splitMessage[i];
+      if (currentLetter == "*") {
+        isSolved = false;
+        return isSolved;
+      }
+    }
+    return isSolved;
+  }
 }
 
 module.exports = Hangman;
